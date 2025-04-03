462e3bc6baedd8c762414ca6647583c5f441bf65
Peter Kotula <peto.kotula@yahoo.com>
2025-04-03 15:40:49 +0200

    parse category response to structs

--- golem_ai_backend/components-rust/golem-ai-input-analyzer/src/lib.rs
+++ golem_ai_backend/components-rust/golem-ai-input-analyzer/src/lib.rs
@@ -6,6 +6,7 @@ use crate::bindings::exports::golem_ai::input_analyzer_exports::golem_ai_input_a
 // use common_lib::example_common_function;
 use common_lib::{ask_openai, get_openai_api_key, OpenAIRequest};
 use std::cell::RefCell;
+use serde::{Deserialize, Serialize};
 
 struct Input {
     input: String,
@@ -26,6 +27,27 @@ thread_local! {
 
 struct InputAnalyzer;
 
+
+fn get_categories_from_json_markdown(json: String) -> Result<Vec<CategoryWithData>, String> {
+    let s = json.strip_prefix("```json").and_then(|s| s.strip_suffix("```")).unwrap_or(&json).trim();
+    let entries: Vec<CategoryWithData> = serde_json::from_str(s).map_err(|e| e.to_string())?;
+    Ok(entries)
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CategoryWithData {
+    pub category: String,
+    pub data: CategoryData,
+}
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CategoryData {
+    pub description: String,
+    pub link: Option<String>
+}
+
+
 pub fn context() -> String {
     "You are a helpful assistant. Your role is to read and extract all the entries present in a Notion document written in markdown. The entries represent potential features or bugfixes or an application. They are categorized in specific sections, that represent the importance of each entry. Some of them are already done, others are in todo or in progress. Collect all the entries. Represent each entry as a JSON object containing, 2 keys: 'category', representing the importance of the entry and 'data', representing all the raw description of the feature or bug, including the nested elements of the entry with any link or code snippet. All those JSON object can be placed in a JSON list. It's very important that you return me only the JSON structure, because I will have to parse your response in JSON.".to_string()
 }
@@ -44,6 +66,7 @@ impl Guest for InputAnalyzer {
 
         match ask_openai(request, get_openai_api_key()).and_then(|r| r.get_message_or_err()) {
             Ok(response) => {
+                println!("RESPONSE: {:?}", get_categories_from_json_markdown(response.clone()));
                 println!("RESPONSE: {}", response.clone());
                 // CONTEXT.with(|ctx| {
                 //     ctx.borrow_mut().history.push(Response {
